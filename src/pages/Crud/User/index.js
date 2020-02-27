8130b3f517e63600e550aacf50f05327fa94fafd
viniciusgoncalves2019 <viniciusgoncalves2019@usp.br>
2020-02-27 19:58:54 -0300

    merge

---
+++ src/pages/Crud/User/index.js
@@ -0,0 +1,261 @@
+import React, { useState, Fragment, useEffect } from 'react';
+import api from '../../../services/api';
+import { MDBDataTable } from 'mdbreact';
+import { FaPlus, FaPen, FaExchangeAlt } from "react-icons/fa";
+import { Button, Modal } from 'react-bootstrap';
+import swal from 'sweetalert';
+import {initialize} from '../../../Util';
+
+export default function User({ history }){
+    const [name, setName] = useState('');
+    const [name, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [cPassword, setCPassword] = useState('');
+    const [user_id, setUserId] = useState('');
+    const [users, setUsers] = useState([]);
+    const [show, setShow] = useState(false);
+    const [change, setChange] = useState(0);
+    const submitButton = React.createRef(null);
+    
+    const handleShow = () => setShow(true);
+    
+    const handleClose = () => { 
+        setShow(false); 
+        setSubjectId(''); 
+        setName('');
+    };
+
+    useEffect((history) =>{
+        document.getElementById("loader").style.display = "block";
+        
+        let auth = initialize({history});
+
+        async function edit(event, id) {
+            event.preventDefault();
+            
+            if(!auth){
+                history.push('/Login');
+            }else{
+    
+                let response = await api.get(`/users/${id}`, {
+                    headers: {authorization: auth[0].authorization},
+                });
+    
+                response = response.data;
+    
+                if(response.result.length > 0){
+                    setName(response.result[0].name);
+                    setEmail(response.result[0].email);
+                    setPassword(response.result[0].password);
+                    setCPassword(response.result[0].password);
+                    setPermission(response.result[0].permission);
+                    setUserId(id);
+                    setShow(true);
+                }
+            }
+        } 
+    
+        async function del(event, id) {
+            event.preventDefault();
+            const willDelete = await swal({
+                title: "Você tem certeza que irá alterar o status de ativação?",
+                icon: "warning",
+                dangerMode: true,
+            });
+            if (willDelete) {
+                let response = await api.delete(`/users/${id}`, 
+                {headers: { authorization: auth[0].authorization }});
+        
+                response = response.data;
+                
+                if(response.result.length > 0){
+                    swal("Alteração realizada com sucesso");
+                    setChange(change+1);
+                }else{
+                    document.getElementById("message").innerHTML = "Não foi possível alterar!";
+                } 
+            }
+        }
+    
+        function returnEditHtml(id){
+            const editHtml = <Fragment>
+                                <button 
+                                    onClick={(e) => {edit(e, id)}} 
+                                    type="button" 
+                                    className="btn btn-warning"><FaPen />
+                                </button>
+                            </Fragment>
+    
+            return editHtml;
+        }
+    
+        function returnDeleteHtml(id){
+            const deleteHtml =  <Fragment>
+                                    <button 
+                                        onClick={(e) => {del(e, id)}}
+                                        type="button" 
+                                        className="btn btn-danger"><FaExchangeAlt />
+                                    </button>
+                                </Fragment>
+    
+            return deleteHtml;
+        }
+
+        async function loadUsers(){
+
+            let response = await api.get('/users');
+
+            response = response.data;
+
+            if(response.result.length > 0){
+                let data = [];
+                for(let i = 0; i < response.result.length; i++){
+                    data.push({
+                        name: response.result[i].name,
+                        active: (response.result[i].active) ? 'Ativo' : 'Inativo',
+                        editar: returnEditHtml(response.result[i].user_id),
+                        status: returnDeleteHtml(response.result[i].user_id)
+                    });
+                }
+                setUsers(data);
+            }
+
+            document.getElementById("loader").style.display = "none";
+        }
+
+        loadUsers();
+    }, [change]);
+
+    
+    const data = {
+        columns: [
+        {
+            label: 'Nome',
+            field: 'name',
+            sort: 'asc',
+        },
+        {
+            label: 'Email',
+            field: 'email',
+            sort: 'asc',
+        },
+        {
+            label: 'Permission',
+            field: 'permission',
+            sort: 'asc',
+        },
+        {
+            label: 'Ativo',
+            field: 'active',
+            sort: 'asc',
+        },
+        {
+            label: 'Editar',
+            field: 'editar',
+            sort: 'asc',
+        },
+        {
+            label: 'Status',
+            field: 'status',
+            sort: 'asc',
+        },
+        ],
+        rows: users
+    };
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        document.getElementById("btn_submit").disabled = true;
+
+        let auth = initialize({history});
+        
+        if(password === cPassword){
+            if(!user_id){
+                let response = await api.post('/users', {name, email, password, permission: 2});
+                response = response.data;
+                
+                if(response.result.length > 0){
+                    handleClose();
+                    swal("Cadastro realizado com sucesso");
+                    setChange(change+1);
+                }else{
+                    document.getElementById("message").innerHTML = "Não foi possível cadastrar!";
+                }
+            }else{
+                let response = await api.put(`/users/${user_id}`, {name, email, password, permission}, 
+                {headers: {authorization: auth}});
+
+                response = response.data;
+
+                if(response.result.length > 0){
+                    handleClose();
+                    swal("Edição realizada com sucesso");
+                    setChange(change+1);
+                }else{
+                    document.getElementById("message").innerHTML = "Não foi possível atualizar cadastro!";
+                }
+            }
+        }else{
+            document.getElementById("message").innerHTML = "Senha diferente da sua confirmação!";
+        }
+        
+    }
+    
+    return (
+        <>
+            <div className="container">
+                <div className="row"></div>        
+                    <div className="centering">
+                        <div className="card card-signin my-5 spacing">
+                            <h3 className="colorTitle">Disciplinas</h3>
+                        <button onClick={handleShow}
+                            className="btn btn-success col-lg-2 tableAdd">
+                            <FaPlus />
+                        </button>  
+                        <MDBDataTable
+                            striped
+                            bordered
+                            small
+                            data={data}
+                        />
+                        </div>
+                    </div>
+            </div>
+
+            <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Disciplina</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form className="form-signin" onSubmit={handleSubmit}>
+                        <div className="form-label-group">
+                            <input type="text" 
+                            id="inputName" 
+                            className="form-control" 
+                            placeholder="Nome da disciplina" required 
+                            value={name}
+                            onChange={event => setName(event.target.value)}/>
+                            <label htmlFor="inputName"> Nome*</label>
+                        </div>
+
+                        <div id="message" align='center'></div>
+
+                        <button ref={submitButton} type="submit" hidden></button>
+                                    
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Fechar
+                    </Button>
+                    <Button variant="primary" id="btn_submit"
+                        onClick={e => { submitButton.current.click(); }}>
+                        Salvar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </> 
+        
+    );
+}
